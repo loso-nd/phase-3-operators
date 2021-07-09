b9c2dfa884ca6c916199ec1aaf3d907d3edb922a
Nahum <losond8@gmail.com>
2021-07-09 16:42:46 -0400

    Ternary Operators.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
+require "pry" 
 def unsafe?(speed)
+    if speed > 60
+        return true
+    end
 
+    if speed < 40
+        return true
+    end
+
+    if (speed > 40) && (speed < 60)
+        return false
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : nil
+    
 end
 	
-
-
