66629e60de2f4ede6015bc81f42c867c2004ed73
Nahum <losond8@gmail.com>
2021-07-09 16:55:26 -0400

    Use of ternary operators.

--- lib/operations.rb
+++ lib/operations.rb
@@ -16,7 +16,17 @@ end
 
 
 def not_safe?(speed)
-	speed < 40 || speed > 60 ? true : nil
+	speed < 40 || speed > 60 ? true : false
     
 end
 	
+
+# def unsafe?(speed)
+#     speed < 40 || speed > 60 ? true : false
+#   end
+  
+#   def not_safe?(speed)
+#     speed < 40 || speed > 60 ? true : false
+#   end
+
+# end
